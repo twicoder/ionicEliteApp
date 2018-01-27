0e9893ca8c35e55f1e138bcb6be1304b1e1c5e63
Chinwe <renqingwei123@163.com>
2018-01-27 22:29:22 +0800

    basic app

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('eliteApp', ['ionic'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -22,3 +22,9 @@ angular.module('starter', ['ionic'])
     }
   });
 })
+
+.controller('MyCtrl',function($scope){
+  $scope.myTitle = "This is the title";
+  $scope.mycontent = "This is the content of app";
+});
+
